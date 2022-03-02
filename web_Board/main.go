f5278d25ddf69fd3b593908e8f9a32a9c3718fe5
Kim-Junggwon <94525599+Kim-Junggwon@users.noreply.github.com>
2022-03-02 18:46:30 +0900

    [mod] db data 콘솔 출력 - 템플릿 출력 추가 예정

--- web_Board/main.go
+++ web_Board/main.go
@@ -6,6 +6,22 @@ import (
 )
 
 func main() {
+	/* 	const dbpath = "test.db"
+	   	d := db.InitDB(dbpath) // db 연동 인스턴스 생성
+	   	defer d.Close()
+
+	   	db.CreateTable(d) // table이 존재하지 않는다면 생성
+
+	   	items := []db.Item{
+	   		db.Item{"0", "T0", "N0", time.Now()},
+	   		db.Item{"1", "T1", "N1", time.Now()},
+	   	}
+	   	db.InsertItem(d, items) // db에 데이터 삽입
+
+	   	read := db.ReadItem(d) // db 데이터 읽기
+	   	for _, v := range read {
+	   		fmt.Println(v.Id, v.Name, v.Title, v.Date.Format("2006-01-02 15:04"))
+	   	} */
 	dbpath := "test.db"
 
 	http.ListenAndServe(":3000", hd.Handler(dbpath))
